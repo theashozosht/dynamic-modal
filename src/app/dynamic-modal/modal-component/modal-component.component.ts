47226cdf8e193ddda4dfa689fd56b48e13a6c5a8
Yasin Satvati <57200251+theashozosht@users.noreply.github.com>
2023-06-15 19:28:16 +0430

    Update modal-component.component.ts
    logic of creation of modal and handling events of it has been developed

--- src/app/dynamic-modal/modal-component/modal-component.component.ts
+++ src/app/dynamic-modal/modal-component/modal-component.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ComponentRef, OnDestroy, ElementRef, ViewChild, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
+import { IModalSchema, IModalSchemaButton } from '../types/modal.interface';
+import { Subject } from 'rxjs';
+import { DialogRef } from '../modal-token/modal.ref'
 
 @Component({
   selector: 'app-modal-component',
   templateUrl: './modal-component.component.html',
-  styleUrls: ['./modal-component.component.scss']
+  styleUrls: ['./modal-component.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ModalComponentComponent {
+export class ModalComponentComponent implements OnDestroy {
+  @ViewChild('modal') modal: ElementRef
 
+  public modalSchema: IModalSchema = {
+    content: '',
+    class: '',
+    icon: '',
+    maxWidth: 700,
+    button: [],
+  };
+  public componentRef: ComponentRef<any>
+  private readonly _onClose = new Subject<any>();
+  public onClose = this._onClose.asObservable();
+
+  constructor(private dialogRef: DialogRef) { }
+
+  ngOnDestroy() {
+    this.dialogRef.close();
+    if (this.componentRef) {
+      this.componentRef.destroy();
+    }
+  }
+
+  onOverlayClicked(evt: any) {
+    if (evt.srcElement.childNodes[0] == this.modal.nativeElement)
+      this.dialogRef.close();
+  }
+
+  close() {
+    this._onClose.next();
+  }
+
+  onDialogClicked(evt: MouseEvent) { evt.stopPropagation() }
 }
