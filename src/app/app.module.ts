22ffc7602ede1f04d24c0efb91cb51ebd87e5ca7
Yasin Satvati <57200251+theashozosht@users.noreply.github.com>
2023-06-25 10:56:33 +0430

    modal module forFeature
    dynamic module with forFeature static member has been created to have a reusable modal module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicModalModule } from './dynamic-modal/dynamic-modal.module';
 import { ModalComponentComponent } from './dynamic-modal/modal-component/modal-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DynamicModalModule.forFeature({
+        title: 'This is a forFeature Header',
+        content: 'This text is displayed in the content of modal and has been passed by schema from app module',
+        class: 'custom-class',
+        icon: '',
+        maxWidth: 700,
+        shouldClose: () => false,
+        button: [
+          {
+            class: 'btn-outline',
+            name: 'Close',
+          },
+          {
+            class: 'btn-fill',
+            name: 'Return',
+          }
+        ]
+
+      })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dynamic-modal/dynamic-modal.module.ts
+++ src/app/dynamic-modal/dynamic-modal.module.ts
@@ -1,12 +1,24 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ModalComponentComponent } from './modal-component/modal-component.component';
+import { ModalServiceService } from './modal-service/modal-service.service';
+import { DialogRef } from './modal-token/modal.ref';
+import { IModalSchema } from './types/modal.interface';
+import { modalConfigToken } from './modal-token/modal.token';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ModalComponentComponent],
+  imports: [CommonModule],
+  providers: [ModalServiceService, DialogRef]
 })
-export class DynamicModalModule { }
+export class DynamicModalModule {
+  static forFeature(config: IModalSchema): ModuleWithProviders<any> {
+    return {
+      ngModule: DynamicModalModule,
+      providers: [
+        { provide: modalConfigToken, useValue: config }
+      ]
+    }
+  }
+}
